d8f6879aa046609d94ab822f488829e07f3f4b68
Musawer Khan <musawerbangash109@gmail.com>
2024-04-22 04:56:29 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+//Variables Method
+//Example 1
+let userName = "Ahmed Bangash";
+userName = "Jani Bangash";
+console.log(userName);
+//Example 2
+let myFavColor = "Black";
+myFavColor = "Navy Blue";
+console.log(myFavColor);
+let userAge = 11;
+userAge = 22;
+console.log(userAge);
---
+++ main.ts
@@ -0,0 +1,18 @@
+    //Variables Method
+     
+    //Example 1
+
+     let userName = "Ahmed Bangash";
+     userName = "Jani Bangash";
+     console.log(userName);
+
+     //Example 2
+    
+     let myFavColor:string = "Black";
+        myFavColor = "Navy Blue"
+        console.log(myFavColor);
+
+     let userAge:number = 11;
+         userAge = 22;
+         console.log(userAge)
+        
